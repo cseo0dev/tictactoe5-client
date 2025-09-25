00b06d969c774d55170a98e5e19b3f03db55dcab
young <seoyoung1025s@gmail.com>
2025-09-25 18:00:00 +0900

    Omok AI 마무리

---
+++ Assets/02. Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[RequireComponent (typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float widthUnit;
+
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera> ();
+        _camera.orthographicSize = widthUnit / _camera.aspect / 2;
+    }
+}
